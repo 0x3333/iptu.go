842f275da3a2e0573de1a133957dddcbfeed0ff8
Tercio Gaudencio Filho <terciofilho@gmail.com>
2016-06-06 20:12:09 -0300

    Funcionando com UI.

--- server/server.go
+++ server/server.go
@@ -37,7 +37,31 @@ type iptu struct {
 }
 
 func (i iptu) String() string {
-	return fmt.Sprintf("numeroContribuinte: %s, docContribuinte1: %s, nomeContribuinte1: %s, docContribuinte2: %s, nomeContribuinte2: %s, nomeLogradouroImovel: %s, numeroImovel: %s, complementoImovel: %s, bairroImovel: %s, referenciaImovel: %s, cepImovel: %s, fracaoIdeal: %f, areaTerreno: %d, areaConstruida: %d, areaOcupada: %d, valorM2Terreno: %f, valorM2Construcao: %f, anoConstrucaoCorrigido: %s, quantidadePavimentos: %d, testadaCalculo: %f, tipoUsoImovel: %s, tipoPadraoConstrucao: %s, tipoTerreno: %s, fatorObsolescencia: %f", i.NumeroContribuinte, i.DocContribuinte1, i.NomeContribuinte1, i.DocContribuinte2, i.NomeContribuinte2, i.NomeLogradouroImovel, i.NumeroImovel, i.ComplementoImovel, i.BairroImovel, i.ReferenciaImovel, i.CepImovel, i.FracaoIdeal, i.AreaTerreno, i.AreaConstruida, i.AreaOcupada, i.ValorM2Terreno, i.ValorM2Construcao, i.AnoConstrucaoCorrigido, i.QuantidadePavimentos, i.TestadaCalculo, i.TipoUsoImovel, i.TipoPadraoConstrucao, i.TipoTerreno, i.FatorObsolescencia)
+	return fmt.Sprintf("numeroContribuinte: %s, docContribuinte1: %s, nomeContribuinte1: %s, docContribuinte2: %s, nomeContribuinte2: %s, nomeLogradouroImovel: %s, numeroImovel: %s, complementoImovel: %s, bairroImovel: %s, referenciaImovel: %s, cepImovel: %s, fracaoIdeal: %f, areaTerreno: %d, areaConstruida: %d, areaOcupada: %d, valorM2Terreno: %f, valorM2Construcao: %f, anoConstrucaoCorrigido: %s, quantidadePavimentos: %d, testadaCalculo: %f, tipoUsoImovel: %s, tipoPadraoConstrucao: %s, tipoTerreno: %s, fatorObsolescencia: %f",
+		i.NumeroContribuinte,
+		i.DocContribuinte1,
+		i.NomeContribuinte1,
+		i.DocContribuinte2,
+		i.NomeContribuinte2,
+		i.NomeLogradouroImovel,
+		i.NumeroImovel,
+		i.ComplementoImovel,
+		i.BairroImovel,
+		i.ReferenciaImovel,
+		i.CepImovel,
+		i.FracaoIdeal,
+		i.AreaTerreno,
+		i.AreaConstruida,
+		i.AreaOcupada,
+		i.ValorM2Terreno,
+		i.ValorM2Construcao,
+		i.AnoConstrucaoCorrigido,
+		i.QuantidadePavimentos,
+		i.TestadaCalculo,
+		i.TipoUsoImovel,
+		i.TipoPadraoConstrucao,
+		i.TipoTerreno,
+		i.FatorObsolescencia)
 }
 
 var (
@@ -68,12 +92,24 @@ func handleAPI() {
 			log.Printf("Error parsing: %s", err.Error())
 		}
 		termos := r.FormValue("termos")
+		termos = strings.Replace(termos, ".", " ", -1)
+		termos = strings.Replace(termos, ",", " ", -1)
+		termos = strings.Replace(termos, "-", " ", -1)
+		termos = strings.Replace(termos, "_", " ", -1)
+		termos = strings.TrimSpace(termos)
 		termosSlice := strings.Split(termos, " ")
-		var termosFT string
+
+		termosFT := ""
 		for _, value := range termosSlice {
-			termosFT += "+" + value + " "
+			// Only search for strings with at least 3 chars
+			if len(value) >= 3 {
+				termosFT += "+" + value + " "
+			}
 		}
-		rows, err := db.Query("SELECT numero_contribuinte,doc_contribuinte_1,nome_contribuinte_1,doc_contribuinte_2,nome_contribuinte_2,nome_logradouro_imovel,numero_imovel,complemento_imovel,bairro_imovel,referencia_imovel,cep_imovel,fracao_ideal,area_terreno,area_construida,area_ocupada,valor_m2_terreno,valor_m2_construcao,ano_construcao_corrigido,quantidade_pavimentos,testada_calculo,tipo_uso_imovel,tipo_padrao_construcao,tipo_terreno,fator_obsolescencia FROM `iptu` WHERE (MATCH(`nome_contribuinte_1`,`nome_contribuinte_2`,`nome_logradouro_imovel`,`referencia_imovel`) AGAINST (? IN BOOLEAN MODE)) UNION SELECT numero_contribuinte,doc_contribuinte_1,nome_contribuinte_1,doc_contribuinte_2,nome_contribuinte_2,nome_logradouro_imovel,numero_imovel,complemento_imovel,bairro_imovel,referencia_imovel,cep_imovel,fracao_ideal,area_terreno,area_construida,area_ocupada,valor_m2_terreno,valor_m2_construcao,ano_construcao_corrigido,quantidade_pavimentos,testada_calculo,tipo_uso_imovel,tipo_padrao_construcao,tipo_terreno,fator_obsolescencia FROM `iptu` WHERE `doc_contribuinte_1` = ? OR `doc_contribuinte_2` = ?", termosFT, termos, termos)
+		if len(termosFT) == 0 {
+			http.Error(w, "Os Termos devem conter ao menos 3 caracteres cada.", http.StatusBadRequest)
+		}
+		rows, err := db.Query("SELECT numero_contribuinte,doc_contribuinte_1,nome_contribuinte_1,doc_contribuinte_2,nome_contribuinte_2,nome_logradouro_imovel,numero_imovel,complemento_imovel,bairro_imovel,referencia_imovel,cep_imovel,fracao_ideal,area_terreno,area_construida,area_ocupada,valor_m2_terreno,valor_m2_construcao,ano_construcao_corrigido,quantidade_pavimentos,testada_calculo,tipo_uso_imovel,tipo_padrao_construcao,tipo_terreno,fator_obsolescencia FROM `iptu` WHERE (MATCH(`nome_contribuinte_1`,`nome_contribuinte_2`,`nome_logradouro_imovel`,`referencia_imovel`) AGAINST (? IN BOOLEAN MODE)) UNION SELECT numero_contribuinte,doc_contribuinte_1,nome_contribuinte_1,doc_contribuinte_2,nome_contribuinte_2,nome_logradouro_imovel,numero_imovel,complemento_imovel,bairro_imovel,referencia_imovel,cep_imovel,fracao_ideal,area_terreno,area_construida,area_ocupada,valor_m2_terreno,valor_m2_construcao,ano_construcao_corrigido,quantidade_pavimentos,testada_calculo,tipo_uso_imovel,tipo_padrao_construcao,tipo_terreno,fator_obsolescencia FROM `iptu` WHERE `doc_contribuinte_1` = ? OR `doc_contribuinte_2` = ? LIMIT 100", termosFT, termos, termos)
 		if err != nil {
 			panic(err.Error())
 		}
@@ -107,11 +143,15 @@ func handleAPI() {
 				&row.FatorObsolescencia)
 			iptus = append(iptus, row)
 		}
-		bytes, err := json.Marshal(&iptus)
-		if err != nil {
-			panic(err.Error())
+		if iptus == nil {
+			w.Write([]byte("[]"))
+		} else {
+			bytes, err := json.Marshal(&iptus)
+			if err != nil {
+				panic(err.Error())
+			}
+			println(string(bytes))
+			w.Write(bytes)
 		}
-		println(string(bytes))
-		w.Write(bytes)
 	})
 }
